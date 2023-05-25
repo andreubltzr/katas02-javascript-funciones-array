88411bbe9ca688f8bb247974f4c6284d49baf3a9
andreubltzr11 <andreubltzr11@gmail.com>
2023-05-25 15:27:28 +0200

    kata

--- src/funcArray.js
+++ src/funcArray.js
@@ -1,16 +1,16 @@
 /* Encuentra el máximo */
 
-export function maxOfTwoNumbers(numero1,numero2){}
-export function findLongestWord(array){}
-export function sumArray(array){}
-export function averageNumbers(array){}
-export function averageWordLength(){}
-export function uniquifyArray(){}
-export function doesWordExist(array, palabra){}
-export function howManyTimes(){}
-export function greatestProduct(){}
+export function maxOfTwoNumbers(numero1, numero2) {
+  return numero1 > numero2 ? numero1 : numero2;
+}
 
-
-
-
-
+export function findLongestWord(array) {}
+export function sumArray(array) {
+  return array;
+}
+export function averageNumbers(array) {}
+export function averageWordLength() {}
+export function uniquifyArray() {}
+export function doesWordExist(array, palabra) {}
+export function howManyTimes() {}
+export function greatestProduct() {}
